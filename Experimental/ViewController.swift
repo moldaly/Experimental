af8eedac407eef1e179f599ae8528a8468c06eca
moldaly <aida.moldaly.2000@mail.ru>
2022-07-25 18:27:56 +0600

    added stashed changes

--- Experimental/ViewController.swift
+++ Experimental/ViewController.swift
@@ -9,6 +9,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var aida: Int?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
